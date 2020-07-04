ea70c94226dbd3c9234b0498a2bd9e394afe07ea
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-07-04 15:13:44 -0600

    updated queryset
    Mal escrito el query

--- backend/ecomerce/api/viewsets.py
+++ backend/ecomerce/api/viewsets.py
@@ -4,7 +4,7 @@ from .models import Direction, User, Followers, Complaints, Currency, Category, 
 from .serializer import DirectionSerializer, UserSerializer, FollowerSerializer, ComplaintSerializer, CurrencySerializer, CategorySerializer, ImageSerializer, ProductSerializer, Image_ProductSerializer, StatusSerializer, ShippingMethodSerializer, paymentMethodSerializer, OrderSerializer, Product_OrderSerializer, Payment_dataSerializer, ActionSerializer, LogSerializer
 
 class DirectionViewSet(viewsets.ModelViewSet):
-    querysets = Direction.objects.all()
+    queryset = Direction.objects.all()
     serializer_class = DirectionSerializer
 
 #Register user
@@ -24,66 +24,66 @@ class UserViewSet(viewsets.ModelViewSet):
     def destroy(self, request, pk=None):
         pass
     """
-    querysets = User.objects.all()
+    queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class FollowersViewSet(viewsets.ModelViewSet):
-    querysets = Followers.objects.all()
+    queryset = Followers.objects.all()
     serializer_class = FollowerSerializer
 
 class ComplaintsViewSet(viewsets.ModelViewSet):
-    querysets = Complaints.objects.all()
+    queryset = Complaints.objects.all()
     serializer_class = ComplaintSerializer
 
 class CurrencyViewSet(viewsets.ModelViewSet):
-    querysets = Currency.objects.all()
+    queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    querysets = Category.objects.all()
+    queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 class ImageViewSet(viewsets.ModelViewSet):
-    querysets = Image.objects.all()
+    queryset = Image.objects.all()
     serializer_class = ImageSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
-    querysets = Product.objects.all()
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ImageProductViewSet(viewsets.ModelViewSet):
-    querysets = Image_Product.objects.all()
+    queryset = Image_Product.objects.all()
     serializer_class = Image_ProductSerializer
 
 class StatusViewSet(viewsets.ModelViewSet):
-    querysets = Status.objects.all()
+    queryset = Status.objects.all()
     serializer_class = StatusSerializer
 
 class ShippingMethodViewSet(viewsets.ModelViewSet):
-    querysets = shipping_method.objects.all()
+    queryset = shipping_method.objects.all()
     serializer_class = ShippingMethodSerializer
 
 class PaymentMethodViewSet(viewsets.ModelViewSet):
-    querysets = payment_method.objects.all()
+    queryset = payment_method.objects.all()
     serializer_class = paymentMethodSerializer
 
 class OrderViewSet(viewsets.ModelViewSet):
-    querysets = Order.objects.all()
+    queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
 class Product_OrderViewSet(viewsets.ModelViewSet):
-    querysets = product_order.objects.all()
+    queryset = product_order.objects.all()
     serializer_class = Product_OrderSerializer
 
 class PaymentDataViewSet(viewsets.ModelViewSet):
-    querysets = payment_data.objects.all()
+    queryset = payment_data.objects.all()
     serializer_class = Payment_dataSerializer
 
 class ActionViewSet(viewsets.ModelViewSet):
-    querysets = Action.objects.all()
+    queryset = Action.objects.all()
     serializer_class = ActionSerializer
 
 class LogViewSet(viewsets.ModelViewSet):
-    querysets = Log.objects.all()
+    queryset = Log.objects.all()
     serializer_class = LogSerializer
