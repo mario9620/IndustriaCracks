7549dcec9fc5da1556a15be406e160374134f936
adonay <adonay16.vz@gmail.com>
2020-07-08 22:49:22 -0600

    componente compras. Cambio a bootstrap 4.5

--- frontend/ecommerce/src/app/app.module.ts
+++ frontend/ecommerce/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ComprasComponent } from './compras/compras.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ComprasComponent
   ],
   imports: [
     BrowserModule,
