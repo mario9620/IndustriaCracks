2b04229ced94f19b6e0de0fa55bee2530ba87c40
adonay <adonay16.vz@gmail.com>
2020-07-09 01:01:04 -0600

    Correcciones en navbar. Rutas.

--- frontend/ecommerce/src/app/app-routing.module.ts
+++ frontend/ecommerce/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ComprasComponent } from './compras/compras.component';
 
+const routes: Routes = [
 
-const routes: Routes = [];
+  { path: 'compras',  component: ComprasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
